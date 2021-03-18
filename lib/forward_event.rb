a15a45882f7e529ec33cfe8b571dee92df39ad50
Brian Wherry <brian.wherry@equilaw.uk.com>
2021-03-18 12:01:09 +0000

    cleanup

--- lib/forward_event.rb
+++ lib/forward_event.rb
@@ -3,4 +3,20 @@
 require_relative "./forward_event/version"
 
 module ForwardEvent
+  INITIALS_REGEX = '(\"(?<initials>.*)\")?'
+  MESSAGE_REGEX = '(?<message>[^\"]*)?'
+  TEMPLATE_REGEX = '(\((?<template_name>.*)\))?'
+  TIME_REGEX = '(\"(?<time>\d{2}:\d{2})\")?'
+  OVCODE_REGEX = '_ov_code>OV\d+)?'
+
+  TIME_OR_OV = "(?<time#{OVCODE_REGEX}#{TIME_REGEX}"
+  MESSAGE_OR_OV = "((?<message#{OVCODE_REGEX}|#{MESSAGE_REGEX})?"
+  INITIALS_OR_OV = "(?<initials#{OVCODE_REGEX}#{INITIALS_REGEX}"
+
+  REGEX_START = "\\A\\[FE(?<days_to_add>\\d+)?(?<days#{OVCODE_REGEX}\\s+"
+  REGEX_END = '\s*\]\z'
+  REGULAR_EXPRESSIONS = [
+    /#{REGEX_START}#{TIME_OR_OV}\s*#{TEMPLATE_REGEX}\s*#{MESSAGE_OR_OV}\s*#{INITIALS_OR_OV}#{REGEX_END}/,
+    /#{REGEX_START}#{MESSAGE_REGEX}#{REGEX_END}/
+  ]
 end
--- lib/forward_event/definition_parse_error.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal
-
-module ForwardEvent
-  class DefinitionParseError < ::StandardError
-    def initialize(msg="definition could not be parsed")
-      super
-    end
-  end
-end
